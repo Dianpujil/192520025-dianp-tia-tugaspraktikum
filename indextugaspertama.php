856f7b6e905dfd491cb58fec092c7225136fa268
Dianpujil <91181164+Dianpujil@users.noreply.github.com>
2021-09-23 18:14:08 +0700

    Add files via upload
    tugas pertama

---
+++ indextugaspertama.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title> Index Tugas Pertama </title>
+        </head>
+        <body>
+            <?php
+            $Nim = 192520025;
+            $nama = "Dian Puji Lestari";
+            $alamat = "Rendeng Gebang";
+            $email = "pujidian002@gmail.com";
+            $bminat = "jaringan";
+            ?>
+    
+            <h2> Data Diri</h2>
+            <img src= "poto.jpg" height="200" width="200">
+            <p>Nim : <?php echo $Nim; ?> </p>
+            <p>Nama : <?php echo $nama; ?> </p>
+            <p>Alamat : <?php echo $alamat; ?> </p>
+            <p>Email : <?php echo $email; ?> </p>
+            <p>Bidang Minat : <?php echo $bminat; ?> </p>
+
+    </body>
+</html>
---
+++ tugaspertama.php
@@ -0,0 +1,22 @@
+<?php
+
+$Nim = 192520025;
+$nama = "Dian Puji Lestari";
+$alamat = "Rendeng Gebang";
+$email = "pujidian002@gmail.com";
+$bminat = "jaringan";
+
+echo "<h2>Data Diri</h2>";
+echo "<br>";
+echo "Nim : ".$Nim;
+echo "<br>";
+echo "Nama : ".$nama;
+echo "<br>";
+echo "Alamat : ".$alamat;
+echo "<br>";
+echo "Alamat Email : ".$email;
+echo "<br>";
+echo "Bidang Minat : ".$bminat;
+echo "<br>";
+
+?>
